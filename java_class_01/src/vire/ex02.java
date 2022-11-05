5e250377876c5f5cff27570f7aa8564758bece1b
1102-22 <1102-22@DESKTOP-32869HS>
2022-11-05 17:17:55 +0900

    1차 수정

--- java_class_01/src/vire/ex02.java
+++ java_class_01/src/vire/ex02.java
@@ -3,7 +3,14 @@ package vire;
 public class ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int price1 = 100;
+		int $price = 200;
+		int _price = 300;
+		System.out.println(price1);
+		System.out.println($price);
+		System.out.println(_price);
+		String firstname = "JAVA";
+		String firstName = "자바";
 
 	}
 
