55415b1488f5575d4a6a5ebd2ac8df5d4e9b5037
1102-22 <1102-22@DESKTOP-32869HS>
2022-11-05 17:00:40 +0900

    1

---
+++ java_class_01/src/vire/ex01.java
@@ -0,0 +1,15 @@
+package vire;
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 2;
+		System.out.println(3 + 2);
+		System.out.println(3 - 2);
+		System.out.println(3 * 2);
+		System.out.println(3 / 2);
+
+	}
+
+}
