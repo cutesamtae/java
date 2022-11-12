f5be61124542c49ac048053ee91b65baef302c0a
1102-22 <1102-22@DESKTOP-32869HS>
2022-11-12 15:37:09 +0900

    20221112_2

--- java_class_01/src/class_04/ex01.java
+++ java_class_01/src/class_04/ex01.java
@@ -1,10 +1,15 @@
 package class_04;
 
+import java.util.Iterator;
+
 public class ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i;
+		for(i=1; i<=5;i++) {
+			System.out.println("For문 안입니다 i : "+ i);
+		}
+		System.out.println("for문을 나왔습니다 i : "+ i);
 	}
 
 }
--- java_class_01/src/class_04/ex02.java
+++ java_class_01/src/class_04/ex02.java
@@ -3,8 +3,11 @@ package class_04;
 public class ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for(int i = 0; i < 10; i++) {
+			for(int j = 0; j < 10; j++) {
+				System.out.println("i:"+ i + ", j:"+j);
+			}
+		}
 	}
 
 }
