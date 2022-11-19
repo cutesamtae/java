b1033da4edeb65e99ec72a06608f026cd7292a23
1102-22 <1102-22@DESKTOP-32869HS>
2022-11-19 19:12:01 +0900

    20221119_1

---
+++ java_class_01/src/class_08/ex01.java
@@ -0,0 +1,28 @@
+package class_08;
+
+class Box {
+	static int boxID = 0;
+	int idNum = 0;
+	public void increase() {
+		boxID++;
+		idNum++;
+	}
+}
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		Box box1 = new Box();
+		Box box2 = new Box();
+		Box box3 = new Box();
+		box1.increase();
+		System.out.println("boxID : " + box1.boxID);
+		System.out.println("idNum : " + box1.idNum);
+		System.out.println();
+		box2.increase();
+		System.out.println("boxID : " + box2.boxID);
+		System.out.println("idNum : " + box2.idNum);
+		System.out.println();
+	}
+
+}
